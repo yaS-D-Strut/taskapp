588afd406181f5106abe8543d34e868117795bf1
yasuaki.osada <dice.worker@gmail.com>
2019-09-15 17:25:41 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Yasuaki Osada on 2019/09/15.
+//  Copyright © 2019 Yasuaki Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
